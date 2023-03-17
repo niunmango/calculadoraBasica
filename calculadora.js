bb7ec39f6e1d85b4c9d55e518728e837fc767787
niunmango <niunmango@yahoo.com>
2023-03-17 14:14:32 -0300

    más básica

--- calculadora.js
+++ calculadora.js
@@ -3,6 +3,19 @@ let operando2 = "";
 let operador = "";
 let resultado = "";
 
+const numero1 = document.getElementById("numero1");
+const numero2 = document.getElementById("numero2");
+const copiarNumeros = document.getElementById("copiarNumeros");
+const numerosCopiados = document.getElementById("numerosCopiados");
+
+copiarNumeros.addEventListener("click", function() {
+  const numero1Valor = numero1.value;
+  const numero2Valor = numero2.value;
+  
+  numerosCopiados.innerHTML = `Número 1: ${numero1Valor}<br>Número 2: ${numero2Valor}`;
+});
+
+
 function agregarNumero(numero) {
   if (operador === "") {
     operando1 += numero;
